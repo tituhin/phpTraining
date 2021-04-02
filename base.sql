ec7da02b49d9b9d2b6ddfc135f418dab0dde65a5
Tariqul Islam Tuhin <tistuhin@outlook.com>
2021-04-02 20:04:57 +0600

    database

--- base.sql
+++ base.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 29, 2021 at 10:12 PM
+-- Generation Time: Apr 02, 2021 at 04:03 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -20,6 +20,52 @@ SET time_zone = "+00:00";
 --
 -- Database: `base`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+
+CREATE TABLE `education` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `degree` varchar(255) DEFAULT NULL,
+  `year` varchar(255) DEFAULT NULL,
+  `percentage` tinyint(3) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `education`
+--
+
+INSERT INTO `education` (`id`, `degree`, `year`, `percentage`) VALUES
+(1, 'Secondary School Certificate', '2012', 82),
+(2, 'Higher Secondary School Certificate', '2014', 86),
+(3, 'B. Sc. In Computer Science & Engineering', '2019', 62);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portfolios`
+--
+
+CREATE TABLE `portfolios` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `featured_image` varchar(255) NOT NULL,
+  `summary` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=Active,2=Inactive;'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `portfolios`
+--
+
+INSERT INTO `portfolios` (`id`, `title`, `category`, `thumbnail`, `featured_image`, `summary`, `status`) VALUES
+(6, 'HAMBLE TRIANGLE', 'DESIGN ', 'thumnail36219_16_pm_Apr_2021.jpg', 'featured_image24919_16_pm_Apr_2021.jpg', '<p><strong>Express</strong> <em>dolor </em>sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus vitae condimem egestliberos dolor auctor tellus, eu consectetur neque elit quis nunc. Cras elementum pretiumi Nullam justo efficitur, trist ligula pellentesque ipsum. Quisque thsr augue ipsum, vehicula tellus maximus. Was popularised in the 1960s withs the release of Letraset sheets containing Lorem Ipsum passags, and more recently with desktop publishing software like Aldus PageMaker including versions.</p>\r\n\r\n<p>Rxpress dolor sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus vitae condimem egestlibers dolosr auctor tellus, eu consectetur neque elit quis nunc. Cras elementum pretiumi Nullam justo efficitur, trist ligula pellentesque ipsum. Quisque thsr augue ipsum, vehicula tellus maximus.</p>\r\n', 1),
+(7, 'HAMBLE TRIANGLE', 'DESIGN ', 'thumnail210403_37_pm_Apr_2021.jpg', 'featured_image18203_37_pm_Apr_2021.jpg', '<p><strong>Express</strong> <em>dolor </em>sit amet, consectetur adipiscing elit. Cras sollicitudin, tellus vitae condimem egestliberos dolor auctor tellus, eu consectetur neque elit quis nunc. Cras elementum pretiumi Nullam justo efficitur, trist ligula pellentesque ipsum. Quisque thsr augue ipsum, vehicula tellus maximus. Was popularised in</p>\r\n', 1);
 
 -- --------------------------------------------------------
 
@@ -51,14 +97,21 @@ INSERT INTO `services` (`id`, `name`, `icon`, `summery`, `status`) VALUES
 
 CREATE TABLE `settings` (
   `id` int(11) NOT NULL,
-  `logo` varchar(255) DEFAULT 'default.png',
+  `logo` varchar(255) DEFAULT 'logo.png',
   `copyright` varchar(255) DEFAULT NULL,
   `tagline` varchar(255) DEFAULT NULL,
   `office_address` varchar(255) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `phone` int(13) DEFAULT NULL,
+  `phone` bigint(20) DEFAULT NULL,
   `about` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`id`, `logo`, `copyright`, `tagline`, `office_address`, `email`, `phone`, `about`) VALUES
+(1, 'logo.png', 'Hello Kufa', 'Professional Web Development with long experience in the field.', 'Dhanmondi, Dhaka, 1205.', 'tarequlislam105150@gmail.com', 8801718404051, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.');
 
 -- --------------------------------------------------------
 
@@ -118,6 +171,18 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `status`, `role`, `profi
 --
 
 --
+-- Indexes for table `education`
+--
+ALTER TABLE `education`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `portfolios`
+--
+ALTER TABLE `portfolios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `services`
 --
 ALTER TABLE `services`
@@ -148,6 +213,18 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `education`
+--
+ALTER TABLE `education`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `portfolios`
+--
+ALTER TABLE `portfolios`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `services`
 --
 ALTER TABLE `services`
@@ -157,7 +234,7 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `socials`
